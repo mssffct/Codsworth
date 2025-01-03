4820fa2916bd801918af4731b85372886d15fe89
Filipp Bushmanov <herooftheday82@mail.ru>
2025-01-03 13:00:41 +0300

    feat(backend): remove old schema file

--- backend/openapi_schema.py
+++
@@ -1,8 +0,0 @@
-from starlette.schemas import SchemaGenerator
-
-schemas = SchemaGenerator(
-    {"openapi": "3.0.0", "info": {"title": "Codsworth API", "version": "1.0"}}
-)
-
-def openapi_schema(request):
-    return schemas.OpenAPIResponse(request=request)
